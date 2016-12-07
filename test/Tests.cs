5c804fe43e79b5b881166d14fb8cf537223034a5
Shaun Martin <martins@surgery.wisc.edu>
2016-12-07 15:14:10 -0600

    Basic setup for unit tests.

---
+++ test/Tests.cs
@@ -0,0 +1,29 @@
+﻿using CoreTemplateWeb.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void IndexReturnsAView() {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
