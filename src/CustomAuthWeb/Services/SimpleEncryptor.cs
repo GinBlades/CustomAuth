2a21442ceedb7ff21b7c4894e21ef771f9e79498
Shaun Martin <webmaster@ginblades.com>
2017-01-21 11:00:19 -0600

    Document encryption and hash classes.
    Any method not currently in use outside the classes have been set to
    private, but I'm not entirely sure what will be used in the future.

--- src/CustomAuthWeb/Services/SimpleEncryptor.cs
+++ src/CustomAuthWeb/Services/SimpleEncryptor.cs
@@ -9,9 +9,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CustomAuthWeb.Services {
+    /// <summary>
+    /// Used to encrypt things like passwords and session variables.
+    /// Currently stores the encrypted message in memory. I may want to avoid that.
+    /// </summary>
     public class SimpleEncryptor {
         private readonly AppSecrets _secrets;
 
+        #region Properties
         private byte[] _secretKey;
         private byte[] _iv;
         private byte[] _encryptedMessage;
@@ -40,11 +45,11 @@ namespace CustomAuthWeb.Services {
             return Convert.ToBase64String(_iv);
         }
 
-        public void SetEncryptedMessage(byte[] message) {
+        private void SetEncryptedMessage(byte[] message) {
             _encryptedMessage = message;
         }
 
-        public void SetEncryptedMessage(string message) {
+        private void SetEncryptedMessage(string message) {
             _encryptedMessage = Convert.FromBase64String(message);
         }
 
@@ -55,22 +60,37 @@ namespace CustomAuthWeb.Services {
         public string GetEncryptedMessageS() {
             return Convert.ToBase64String(_encryptedMessage);
         }
+        #endregion Properites
 
         private readonly RandomNumberGenerator _random = RandomNumberGenerator.Create();
         public readonly int BlockBitSize = 128;
         public readonly int KeyBitSize = 256;
 
+        /// <summary>
+        /// Inject AppSecrets configuration which holds the SecretKey property, used as AES encryption key
+        /// </summary>
+        /// <param name="secrets">Configuration option loaded from Startup using DI</param>
         public SimpleEncryptor(IOptions<AppSecrets> secrets) {
             _secrets = secrets.Value;
             SetSecretKey(_secrets.SecretKey);
         }
 
-        public byte[] NewKey() {
+        /// <summary>
+        /// This helper can be used to generate a key of the appropriate size, which should then be saved in a secret config file.
+        /// </summary>
+        /// <returns>New Random key</returns>
+        private byte[] NewKey() {
             var key = new byte[KeyBitSize / 8];
             _random.GetBytes(key);
             return key;
         }
 
+        /// <summary>
+        /// Takes a plain text message and encrypts it using AES encryption with the SecretKey.
+        /// An IV may be provided, for instances where the value will be hashed and it is necessary to generate the same value.
+        /// </summary>
+        /// <param name="plainText">Plain text string.</param>
+        /// <param name="existingIv">IV if needed to match encrypted value to stored value.</param>
         public void Encrypt(string plainText, byte[] existingIv = null) {
             using (var aes = Aes.Create()) {
                 if (existingIv != null) {
@@ -92,11 +112,21 @@ namespace CustomAuthWeb.Services {
             }
         }
 
+        /// <summary>
+        /// Generates a string which can be stored in the database or a cookie and decrypted later.
+        /// </summary>
+        /// <param name="plainText">Plain text string.</param>
+        /// <param name="existingIv">IV if needed to match encrypted value to stored value.</param>
+        /// <returns>String in the format [encryptedMessage].stm.[IV]</returns>
         public string EncryptToString(string plainText, byte[] existingIv = null) {
             Encrypt(plainText, existingIv);
             return StmJoiner(new string[] { GetEncryptedMessageS(), GetIVS() });
         }
 
+        /// <summary>
+        /// Expects _secretKey and _iv to be set already using the appropriate setters.
+        /// </summary>
+        /// <returns>String containing the decrypted message</returns>
         public string Decrypt() {
             string plainText = null;
             using (var aes = Aes.Create()) {
@@ -113,6 +143,11 @@ namespace CustomAuthWeb.Services {
             return plainText;
         }
 
+        /// <summary>
+        /// Takes a string that may have been stored in the database and concatenated using the 'EncryptToSTring' method.
+        /// </summary>
+        /// <param name="messageWithIv">String in the format [encryptedMessage].stm.[IV]</param>
+        /// <returns>String containing the decrypted message</returns>
         public string DecryptFromString(string messageWithIv) {
             string[] parts = StmSplitter(messageWithIv);
             // TODO: Add checks on parts
@@ -121,10 +156,20 @@ namespace CustomAuthWeb.Services {
             return Decrypt();
         }
 
+        /// <summary>
+        /// Helper used here and by the hash method.
+        /// </summary>
+        /// <param name="concatenatedString">A string in the format [value].stm.[value]</param>
+        /// <returns>A string array of the pieces that were concatenated.</returns>
         public string[] StmSplitter(string concatenatedString) {
             return concatenatedString.Split(new string[] { ".stm." }, StringSplitOptions.None);
         }
 
+        /// <summary>
+        /// Helper used here and by the hash method.
+        /// </summary>
+        /// <param name="strings">An array of strings to concatenate</param>
+        /// <returns>A string in the format [value].stm.[value]</returns>
         public string StmJoiner(string[] strings) {
             return string.Join(".stm.", strings);
         }
--- src/CustomAuthWeb/Services/SimpleHasher.cs
+++ src/CustomAuthWeb/Services/SimpleHasher.cs
@@ -6,38 +6,68 @@ using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace CustomAuthWeb.Services {
+    /// <summary>
+    /// The primary purpose of this class is to hash and compare hashed passwords with the 'HashWithEncryption'
+    /// and 'CompareWithEncryption' methods. The other methods may be useful in other areas, but for now they are marked
+    /// as private so that its clear they are not currently used.
+    /// </summary>
     public class SimpleHasher {
         private readonly SimpleEncryptor _encryptor;
 
+        #region SaltProperties
         private byte[] _salt;
 
-        public void SetSalt() {
+        /// <summary>
+        /// Sets a random byte array to the appropriate length for a salt and uses a random number generator to fill the bytes
+        /// with random values.
+        /// </summary>
+        private void SetSalt() {
             _salt = new byte[128 / 8];
             using (var rng = RandomNumberGenerator.Create()) {
                 rng.GetBytes(_salt);
             }
         }
 
-        public void SetSalt(byte[] salt) {
-            _salt = salt;
+        private void SetSalt(byte[] salt) {
+            if (salt == null) {
+                SetSalt();
+            } else {
+                _salt = salt;
+            }
         }
 
-        public void SetSalt(string salt) {
-            _salt = Convert.FromBase64String(salt);
+        private void SetSalt(string salt) {
+            if (salt == null) {
+                SetSalt();
+            } else {
+                _salt = Convert.FromBase64String(salt);
+            }
         }
 
-        public byte[] GetSaltBA() {
+        private byte[] GetSaltBA() {
             return _salt;
         }
 
-        public string GetSaltS() {
+        private string GetSaltS() {
             return Convert.ToBase64String(_salt);
         }
+        #endregion SaltProperties
 
+        /// <summary>
+        /// Inject SimpleEncryptor for use with passwords that must be encrypted before being hashed.
+        /// </summary>
         public SimpleHasher(SimpleEncryptor encryptor) {
             _encryptor = encryptor;
         }
 
+        #region HashOverloads
+        /// <summary>
+        /// From https://docs.microsoft.com/en-us/aspnet/core/security/data-protection/consumer-apis/password-hashing
+        /// Allows a salt to be passed in for matching existing hashes.
+        /// </summary>
+        /// <param name="password">String to be used as a password or some other secret</param>
+        /// <param name="salt">Byte array of length (128 / 8)</param>
+        /// <returns>Byte[] representing the hashed password.</returns>
         public byte[] Hash(string password, byte[] salt) {
             SetSalt(salt);
             return KeyDerivation.Pbkdf2(
@@ -58,7 +88,14 @@ namespace CustomAuthWeb.Services {
             SetSalt(salt);
             return Hash(password, _salt);
         }
+        #endregion HashOverloads
 
+        /// <summary>
+        /// First encrypts the password using the secret key, then hashes it.
+        /// The string returned can be split to get all the required parameters for matching against the existing hash.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns>A string in the format [hash].stm.[encryptionIV].stm.[salt]</returns>
         public string HashWithEncryption(string password) {
             _encryptor.Encrypt(password);
             var hash = Hash(_encryptor.GetEncryptedMessageS());
@@ -69,6 +106,13 @@ namespace CustomAuthWeb.Services {
             });
         }
 
+        /// <summary>
+        /// Takes in a new guess string and matches it against an existing password. The password is expected to be in the
+        /// format provided by 'HashWithEncryption'
+        /// </summary>
+        /// <param name="guess">Any string</param>
+        /// <param name="password">String in the format [hash].stm.[encryptionIV].stm.[salt]</param>
+        /// <returns>Returns true if guess matches existing password, otherwise false.</returns>
         public bool CompareWithEncryption(string guess, string password) {
             // TODO: Validate parts
             string[] parts = _encryptor.StmSplitter(password);
@@ -78,12 +122,29 @@ namespace CustomAuthWeb.Services {
             return Compare(parts[0], Convert.ToBase64String(hashedGuess));
         }
 
-        // TODO: Prevent returning early to not give unnecessary info
-        public bool Compare(string stringA, string stringB) {
-            return ConstantTimeComparison(Convert.FromBase64String(stringA), Convert.FromBase64String(stringB);
+        /// <summary>
+        /// Based on byte[] version found on web.
+        /// </summary>
+        /// <param name="stringA"></param>
+        /// <param name="stringB"></param>
+        /// <returns></returns>
+        private bool Compare(string stringA, string stringB) {
+            uint difference = (uint)Math.Abs(stringA.Length - stringB.Length);
+            for (var i = 0; i < stringA.Length && i < stringB.Length; i++) {
+                difference += (uint)Math.Abs(stringA[i].CompareTo(stringB[i]));
+            }
+            return difference == 0;
         }
 
-        private static bool ConstantTimeComparison(byte[] passwordGuess, byte[] actualPassword) {
+        /// <summary>
+        /// From https://lockmedown.com/hash-right-implementing-pbkdf2-net/
+        /// This prevents an attacker from inferring when they got a set of characters correct.
+        /// Uses bit operators to compare each item in the list
+        /// </summary>
+        /// <param name="passwordGuess"></param>
+        /// <param name="actualPassword"></param>
+        /// <returns></returns>
+        private bool Compare(byte[] passwordGuess, byte[] actualPassword) {
             uint difference = (uint)passwordGuess.Length ^ (uint)actualPassword.Length;
             for (var i = 0; i < passwordGuess.Length && i < actualPassword.Length; i++) {
                 difference |= (uint)(passwordGuess[i] ^ actualPassword[i]);
